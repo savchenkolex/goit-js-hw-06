e98d131506cdf34853a56fa7de96f2730bee3eef
lex <savchenko.lex@gmail.com>
2023-05-14 20:08:38 +0300

    create li elements from array

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = ingredients.map((el) => {
+  const listEl = document.createElement("li");
+  listEl.textContent = el;
+  listEl.classList.add("item");
+  return listEl;
+})
+document.querySelector("#ingredients").append(...ingredientsList);
