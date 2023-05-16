61fc72b8eaf2efa9fb5eea3be215d08bd87865f1
lex <savchenko.lex@gmail.com>
2023-05-16 16:37:36 +0300

    Task 5 completed. input event listener

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
+inputEl.addEventListener("input", keyboardHandler);
+
+function keyboardHandler(event) {
+    if (inputEl.value){
+    outputEl.textContent = event.currentTarget.value;
+    } else {
+    outputEl.textContent = "Anonymous";
+    }
+}
+
+
