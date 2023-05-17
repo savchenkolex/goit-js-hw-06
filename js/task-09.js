2c88a94822de543f39c05ff5588066364520ac85
lex <savchenko.lex@gmail.com>
2023-05-17 18:39:16 +0300

    completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+
+buttonEl.addEventListener("click", buttonChangeColorHendler);
+
+function buttonChangeColorHendler(event){
+  const nextColor = getRandomHexColor();
+  if(event){
+
+    document.querySelector("body").style.backgroundColor = nextColor;
+    document.querySelector(".color").textContent = nextColor;
+  }
+}
